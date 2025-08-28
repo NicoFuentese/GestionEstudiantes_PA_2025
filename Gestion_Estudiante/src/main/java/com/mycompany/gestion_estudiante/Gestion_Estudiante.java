702a066e0207e2a4026f34564691bae7ad75f5c5
Your Name <eduardo.sandoval.r@mail.pucv.cl>
2025-08-27 21:14:10 -0400

    lector

--- Gestion_Estudiante/src/main/java/com/mycompany/gestion_estudiante/Gestion_Estudiante.java
+++ Gestion_Estudiante/src/main/java/com/mycompany/gestion_estudiante/Gestion_Estudiante.java
@@ -8,9 +8,13 @@ package com.mycompany.gestion_estudiante;
  *
  * @author Nicolas
  */
+import java.io.*;
 public class Gestion_Estudiante {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
+        
+        BufferedReader lector = new BufferedReader( new InputStreamReader( System.in));
+        
         System.out.println("Hello World!");
     }
 }
