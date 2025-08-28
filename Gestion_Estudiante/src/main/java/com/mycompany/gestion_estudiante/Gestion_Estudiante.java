de5ee3899dcef7a341a5a90759f0d917428997f5
NicoFuentese <nicolas.a.fuentes.e@gmail.com>
2025-08-27 22:21:36 -0400

    Principio del main
    inicio del menu principal.

--- Gestion_Estudiante/src/main/java/com/mycompany/gestion_estudiante/Gestion_Estudiante.java
+++ Gestion_Estudiante/src/main/java/com/mycompany/gestion_estudiante/Gestion_Estudiante.java
@@ -10,11 +10,33 @@ package com.mycompany.gestion_estudiante;
  */
 import java.io.*;
 public class Gestion_Estudiante {
-
+    
+    private static BufferedReader lector = new BufferedReader( new InputStreamReader( System.in));
+    
     public static void main(String[] args) throws IOException{
         
-        BufferedReader lector = new BufferedReader( new InputStreamReader( System.in));
+        Colegio colegio = Colegio.demo();
+        int opcion;
         
-        System.out.println("Hello World!");
+        do{
+            System.out.println("\n=== SISTEMA GESTIÓN ESTUDIANTES ===");
+            System.out.println("1) Insertar Alumno en un Nivel (manual)");
+            System.out.println("2) Listar Alumnos por Nivel");
+            System.out.println("3) Listar Niveles"); 
+            System.out.println("0) Salir"); 
+            System.out.println("Opcion: "); 
+            opcion = leerEntero();
+        } while(opcion != 0);
+    }
+    
+    public static int leerEntero()throws IOException {
+        while(true) {
+            String entrada = lector.readLine();
+            if (entrada.trim().matches("[0-9]+")) {
+                return Integer.parseInt(entrada);
+            } else {
+                System.out.println("No es un numero, ingreselo nuevamente");
+            }
+        }
     }
 }
