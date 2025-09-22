e55b4762a8d0869a364298df205fa5ca57f8dec1
NicoFuentese <nicolas.a.fuentes.e@gmail.com>
2025-09-22 16:56:42 -0300

    correccion inscripciones
    nombre mal escrito al principio

--- Gestion_Estudiante/src/main/java/com/mycompany/gestion_estudiante/Nivel.java
+++ Gestion_Estudiante/src/main/java/com/mycompany/gestion_estudiante/Nivel.java
@@ -21,7 +21,7 @@ public class Nivel {
     private String paralelo;
     private int cantidadMaximaAlumnos;
     private boolean activo;
-    private List<Inscripcion> insripciones = new ArrayList<>();
+    private List<Inscripcion> inscripciones = new ArrayList<>();
 
     // Constructores
     public Nivel() {}
